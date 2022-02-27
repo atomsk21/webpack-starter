5fac87a4c80bf2b1cf7738b5c0066e68306dd1d1
Luis <luis.m.pinilla@gmail.com>
2022-02-27 14:47:06 -0300

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { saludar } from "./js/componentes.js";
+import "./css/styles.css";
+
+const nombre = "Luis";
+saludar(nombre);
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebPack = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+
+  output: {
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+        options: {
+          sources: false,
+        },
+      },
+      {
+        test: /\.css$/i,
+        exclude: /styles.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /styles.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|jp?g|gif)$/i,
+        loader: "file-loader",
+      },
+    ],
+  },
+  optimization: {},
+
+  plugins: [
+    new HtmlWebPack({
+      title: "Mi Webpack App",
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      ignoreOrder: false,
+    }),
+    new CopyPlugin({ patterns: [{ from: "src/assets/", to: "assets/" }] }),
+  ],
+};
